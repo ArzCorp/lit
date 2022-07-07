5beb9a160b17709c674366b2f04378ac827ae501
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-07-06 22:06:14 -0500

    🔥 removed files

--- src/components/bindings/template.js
+++
@@ -1,8 +0,0 @@
-import { html, render } from '/node_modules/lit-html/lit-html.js'
-
-const otherTemplate = html`<p>SOY OTRO TEMPLATE</p>`
-
-const bindingTemplate = () =>
-	html`<div class="container">Soy template padre ${otherTemplate}</div> `
-
-render(bindingTemplate(), document.querySelector('#bindings-template'))
--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
-import './components/title.js'
-import './components/Wallet.js'
 import './components/List.js'
 import './components/GetData.js'
